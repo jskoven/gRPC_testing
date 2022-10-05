6442f83cedcc8c4106a63aa185cbe176d2379b64
Jklo <jklo@itu.dk>
2022-10-05 15:20:18 +0200

    commit

--- chat/chat.go
+++ chat/chat.go
@@ -2,6 +2,7 @@ package gRPC_testing
 
 import (
 	"log"
+	"time"
 
 	"golang.org/x/net/context"
 )
@@ -13,5 +14,5 @@ type Server struct {
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
 	log.Printf("Received message body from client: %s", message.Body)
 	log.Printf("Received time from client: %s", message.TimeNow)
-	return &Message{Body: "Hello from server!"}, nil
+	return &Message{Body: "Server received message and time. Server time is: ", TimeNow: time.Now().String()}, nil
 }
